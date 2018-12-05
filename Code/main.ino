5c139425f38210b4a966b8f0d9208ad2508b0436
Andreas Rogl <45318504+andreasrogl@users.noreply.github.com>
2018-12-05 15:44:00 +0100

    Update main.ino

--- Code/main.ino
+++ Code/main.ino
@@ -1,1 +1,28 @@
 
+//geschwindigkeit: wie schnell der Roboter fahren soll, bei 0 maximale gschw.
+//strecke: wie weit der Roboter fahren soll, bei 0 bleibt am fleck
+//grad: wie stark sich der Roboter drehen soll, bei 0 Geradeaus
+void fahen (double geschwindigkeit, double stecke, double grad)
+{
+  
+}
+
+//wie stark sich der Roboter drehen soll
+void drehen (double grad)
+{
+  
+}
+
+//schaut mit abstandssensoren nach gegner, wenn erkannt, dann true
+boolean roboterErkennen ()
+{
+  
+}
+
+//wenn der Roboter auf einer Linie ist, dann true
+boolean aufLinie ()
+{
+  
+}
+  
+
