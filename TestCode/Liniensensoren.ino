18ea359a6e8b1d56919855bceb6ef915c6e4fe86
lilau2 <45618564+lilau2@users.noreply.github.com>
2019-01-09 15:44:59 +0100

    Add files via upload
    Lichtsensorenauslesen. OHNE Motor ansteuerung.

---
+++ TestCode/Liniensensoren.ino
@@ -0,0 +1,61 @@
+int LSensor[4] = {3, 4, 5, 6};
+int LSensor_Stat[4];
+
+boolean Linie(int Sensor)
+{
+  if (digitalRead(LSensor[Sensor]) == 0)
+  {
+    Serial.print("1");
+    return 1;
+  } else {
+    Serial.print("0");
+    return 0;
+  }
+
+}
+void setup() {
+  Serial.begin(9600);
+  pinMode(LSensor, INPUT);
+
+}
+void loop() {
+  Linie(0);
+  Linie(1);
+  Linie(2);
+  Linie(3);
+  Serial.println("");
+  /*
+    if (digitalRead(LSensor[0]) == 0)
+    {
+      Serial.print("1");
+
+    } else {
+      Serial.print("0");
+
+    }
+    if (digitalRead(LSensor[1]) == 0)
+    {
+      Serial.print("1");
+
+    } else {
+      Serial.print("0");
+
+    }
+    if (digitalRead(LSensor[2]) == 0)
+    {
+      Serial.print("1");
+
+    } else {
+      Serial.print("0");
+
+    }
+    if (digitalRead(LSensor[3]) == 0)
+    {
+      Serial.println("1");
+
+    } else {
+      Serial.println("0");
+
+    }
+  */
+}
