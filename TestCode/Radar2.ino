b7c934114325121c4b9a94fcdcb10a35051408c7
WombatGladiators <45430520+WombatGladiators@users.noreply.github.com>
2019-03-26 16:16:34 +0100

    Update Radar2.ino
    library hinzugefügt

--- TestCode/Radar2.ino
+++ TestCode/Radar2.ino
@@ -1,7 +1,13 @@
 #include <Servo.h>
+#include <NewPing.h>
+
+#define TRIGGER_PIN  10  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 int degree = 15;
 int hoch = 1;
+int list[200];
 
 unsigned long oldMillis = millis + 2000;
 
@@ -9,8 +15,10 @@ Servo myServo;
 
 const int trigPin = 10;
 const int echoPin = 11;
-unsigned long timespan;
 float distance;
+
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
   Serial.begin(9600);
@@ -19,35 +27,33 @@ void setup() {
 
 void loop() {
   unsigned long currMillis = millis;
-
+  
   if ((hoch == 1) && (oldMillis >= currMillis)) {
-    degree++;
+    degree+=10;
     if (degree > 165) {
       hoch = 0;
     }
   } else {
-    degree--;
+    degree-=10;
     if (degree < 15) {
       hoch = 1;
     }
   }
   myServo.write(degree);
-    Serial.print(degree);
-    Serial.print(" , ");
-    Serial.println(calculateDistance());
-    Serial.println(timespan);
+  distance=calculateDistance();
+    list[degree]=distance;
   oldMillis = currMillis + 10;
 }
 
 float calculateDistance() {
 
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  timespan = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
-  distance = timespan * 0.034 / 2;
+  int distance=sonar.ping_cm();
+  if(distance>0){
+  Serial.println(distance); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  
+  }else{
+    distance=500;
+    }
+  delay(35);
   return distance;
 }
